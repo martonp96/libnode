7c72e94246ba280c367d09812cc01c7839b04b70
Vektor <whiteman.chr@hotmail.com>
2024-04-19 14:13:15 +0200

    test action

--- packages/libnode.lua
+++ packages/libnode.lua
@@ -15,18 +15,22 @@ package("libnode")
         io.replace("tools/v8_gypfiles/features.gypi", "'v8_advanced_bigint_algorithms%%': 1", "'v8_advanced_bigint_algorithms%%': 1,\n    'use_rtti%%': 1")
 
         if package:is_plat("windows") then
-            local configs = { "release", "x64", "dll", "no-cctest" }
+            local configs = { is_mode("debug") and "debug" or "release", "x64", "dll", "no-cctest" }
+            print(is_mode("debug") and "debug" or "release")
             os.vrunv("vcbuild.bat", configs)
         else
-            local configs = { "--shared", "--prefix=" .. package:installdir("rel") }
+            local configs = { is_mode("debug") and "--debug" or "", "--shared", "--prefix=" .. package:installdir("rel") }
             os.vrunv("./configure", configs)
             os.vrunv("make", { "-j4" })
         end
-        os.trycp("out/Release/*.dylib", package:installdir("lib"))
         os.trycp("out/Release/*.so", package:installdir("bin"))
-        os.trycp("out/Release/*.a", package:installdir("lib"))
         os.trycp("out/Release/*.lib", package:installdir("lib"))
         os.trycp("out/Release/*.dll", package:installdir("bin"))
+        os.trycp("out/Release/*.pdb", package:installdir("bin"))
+        os.trycp("out/Debug/*.so", package:installdir("bin"))
+        os.trycp("out/Debug/*.lib", package:installdir("lib"))
+        os.trycp("out/Debug/*.dll", package:installdir("bin"))
+        os.trycp("out/Debug/*.pdb", package:installdir("bin"))
         os.trycp("src/**.h", package:installdir("include"), { rootdir="src" })
         os.trycp("deps/openssl/**.h", package:installdir("deps", "openssl"), { rootdir="deps/openssl" })
         os.trycp("deps/uv/include/**.h", package:installdir("deps", "uv", "include"), { rootdir="deps/uv/include" })
