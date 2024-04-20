4c72a9c4a3b1259ba184c143855e06dee7a98178
Vektor <whiteman.chr@hotmail.com>
2024-04-20 12:41:22 +0200

    add more info, update action versions

--- packages/libnode.lua
+++ packages/libnode.lua
@@ -10,13 +10,16 @@ package("libnode")
     end)
 
     on_install("macosx", "linux", "windows", function (package)
-        io.replace("common.gypi", "'-fno-rtti',", "", { plain = true })
+        -- reenable internal module use
+        io.replace("lib/internal/bootstrap/realm.js", "StringPrototypeStartsWith(id, 'internal/')", "StringPrototypeStartsWith(id, 'yespls/')", { plain = true })
+        -- don't add version suffix to the so file
         io.replace("configure.py", "    shlib_suffix = 'so.%s'", "    shlib_suffix = 'so'", { plain = true })
+        -- enable RTTI for the build
+        io.replace("common.gypi", "'-fno-rtti',", "", { plain = true })
         io.replace("tools/v8_gypfiles/features.gypi", "'v8_advanced_bigint_algorithms%%': 1", "'v8_advanced_bigint_algorithms%%': 1,\n    'use_rtti%%': 1")
 
         if package:is_plat("windows") then
             local configs = { is_mode("debug") and "debug" or "release", "x64", "dll", "no-cctest" }
-            print(is_mode("debug") and "debug" or "release")
             os.vrunv("vcbuild.bat", configs)
         else
             local configs = { "--shared", "--prefix=" .. package:installdir("rel") }
@@ -26,14 +29,12 @@ package("libnode")
             os.vrunv("./configure", configs)
             os.vrunv("make", { "-j4" })
         end
-        os.trycp("out/Release/*.so", package:installdir("bin"))
-        os.trycp("out/Release/*.lib", package:installdir("lib"))
-        os.trycp("out/Release/*.dll", package:installdir("bin"))
-        os.trycp("out/Release/*.pdb", package:installdir("bin"))
-        os.trycp("out/Debug/*.so", package:installdir("bin"))
-        os.trycp("out/Debug/*.lib", package:installdir("lib"))
-        os.trycp("out/Debug/*.dll", package:installdir("bin"))
-        os.trycp("out/Debug/*.pdb", package:installdir("bin"))
+
+        local type = is_mode("debug") and "Debug" or "Release"
+        os.trycp("out/" .. type .. "/libnode.so", package:installdir("bin"))
+        os.trycp("out/" .. type .. "/libnode.lib", package:installdir("bin"))
+        os.trycp("out/" .. type .. "/libnode.dll", package:installdir("bin"))
+        os.trycp("out/" .. type .. "/libnode.pdb", package:installdir("bin"))
         os.trycp("src/**.h", package:installdir("include"), { rootdir="src" })
         os.trycp("deps/openssl/**.h", package:installdir("deps", "openssl"), { rootdir="deps/openssl" })
         os.trycp("deps/uv/include/**.h", package:installdir("deps", "uv", "include"), { rootdir="deps/uv/include" })
