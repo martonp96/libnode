71d836e1eda803334c76592cb554134bbc762d56
Vektor <whiteman.chr@hotmail.com>
2024-04-26 14:27:13 +0200

    fix

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
 cd node
 
-git apply --reject --whitespace=fix ../patches/enable_rtti.patch
-git apply --reject --whitespace=fix ../patches/internal_modules.patch
-git apply --reject --whitespace=fix ../patches/rename_output.patch
-git apply --reject --whitespace=fix ../patches/version_suffix.patch
+git --version
 
-vcbuild.bat release x64 dll no-cctest
+git apply --reject --whitespace=fix --directory=node ../patches/enable_rtti.patch
+git apply --reject --whitespace=fix --directory=node ../patches/internal_modules.patch
+git apply --reject --whitespace=fix --directory=node ../patches/rename_output.patch
+git apply --reject --whitespace=fix --directory=node ../patches/version_suffix.patch
+
+call vcbuild.bat release x64 dll no-cctest
--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ git apply --reject --whitespace=fix ../patches/internal_modules.patch
 git apply --reject --whitespace=fix ../patches/rename_output.patch
 git apply --reject --whitespace=fix ../patches/version_suffix.patch
 
-configure --shared
+./configure --shared
 make -j4 VARIATION="musl"
