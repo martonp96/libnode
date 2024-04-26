ae8774e43863fe6e2eda88320166b779039b9853
Vektor <whiteman.chr@hotmail.com>
2024-04-26 14:35:26 +0200

    fix

--- build.sh
+++
@@ -1,9 +0,0 @@
-cd node
-
-git apply --reject --whitespace=fix ../patches/enable_rtti.patch
-git apply --reject --whitespace=fix ../patches/internal_modules.patch
-git apply --reject --whitespace=fix ../patches/rename_output.patch
-git apply --reject --whitespace=fix ../patches/version_suffix.patch
-
-./configure --shared
-make -j4 VARIATION="musl"
